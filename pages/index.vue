2d733c3689b39a8ce19be46c3e9d295365d522da
horisaki <seiji.horisaki@gmail.com>
2018-11-17 16:25:36 +0900

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,112 @@
+<template>
+  <section>
+    <sample-component :title="indexTitle">
+      Hello Slot!
+      <div slot="named">
+        named slot
+      </div>
+      <div slot="example">
+        example slot
+      </div>
+    </sample-component>
+    <nuxt-link to="/about">ABOUT</nuxt-link>
+  </section>
+</template>
+
+<script>
+import SampleComponent from '~/components/SampleComponent.vue'
+
+export default {
+  components: {
+    SampleComponent
+  },
+  data() {
+    return {
+      indexTitle: 'Nuxt.js data'
+    }
+  },
+  head() {
+    return {
+      title: 'TOP'
+    }
+  }
+}
+</script>
+
+<!-- <template>
+  <section class="container">
+    <div>
+      <logo/>
+      <h1 class="title">
+        portfolio
+      </h1>
+      <h2 class="subtitle">
+        My groundbreaking Nuxt.js project
+      </h2>
+      <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          class="button--green">Documentation</a>
+        <a<template>
+  <sample-component />
+</template>
+
+<script>
+import SampleComponent from '~/components/SampleComponent.vue'
+
+export default {
+  components: {
+    SampleComponent
+  }
+}
+</script>
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          class="button--grey">GitHub</a>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style> -->
